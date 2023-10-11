bcccc9ad4e5b07423d65ea6facbd899a0b816855
SrLavor <srlavor@hotmail.com>
2023-10-11 15:04:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Teste')
 fps = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
 while True:
     #event loop, checa todos eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit() # termina while para que o codigo não continue
+
+    # desenha na tela (main surface) a surface (test_surface) na posição x0 y0
+    screen.blit(test_surface, (0,0))
+
     #desenha todos elementos e atualiza tudo
     pygame.display.update()
     # limita jogo a 60 fps
